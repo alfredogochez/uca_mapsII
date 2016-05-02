be87d82bfb57a65c0283dd5304318131b14e6766
alfredogochez <00040211@uca.edu.sv>
2016-05-01 20:00:43 -0600

    cambios para el guardado de rutas

--- ucampas/app/src/main/java/zero/ucamaps/DialogFavoriteList.java
+++ ucampas/app/src/main/java/zero/ucamaps/DialogFavoriteList.java
@@ -25,10 +25,7 @@ import zero.ucamaps.R;
  * Created by alf on 23/04/2016.
  */
 public class DialogFavoriteList extends DialogFragment {
-    //lista quemada de strings, con el adaptador para cuando la lista sea dinamica
-    ListView lista;
-    ArrayAdapter<String> adaptador;
-    //dialogo de la lista
+
     @Override
 
     public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -38,32 +35,24 @@ public class DialogFavoriteList extends DialogFragment {
         listaRutas = recuperar();
 
         //lista = (ListView) getActivity().findViewById(R.id.lista_favoritos);
-        adaptador = new ArrayAdapter<String>(getActivity(),R.layout.favorites, listaRutas);
+        //adaptador = new ArrayAdapter<String>(getActivity(),R.layout.favorites, listaRutas);
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = getActivity().getLayoutInflater();
         //adaptador = new ArrayAdapter<String>(getActivity().getApplicationContext(), R.layout.favorites, listaRutas);
 
         if(listaRutas.length <= 0){
             //No habian rutas
-            //Toast.makeText(getActivity(), "No hay rutas guardadas como favoritas", Toast.LENGTH_SHORT).show();
-            builder.setMessage("No hay rutas marcadas como favoritas");
+            Toast.makeText(getActivity(), "No hay rutas guardadas como favoritas", Toast.LENGTH_SHORT).show();
+            //builder.setMessage("No hay rutas marcadas como favoritas");
             return builder.create();
         }
         else{
             builder.setView(inflater.inflate(R.layout.favorites, null))
                     .setTitle("Rutas Favoritas")
-                    /*.setItems(listaRutas, new DialogInterface.OnClickListener() {
+                    .setItems(listaRutas, new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
                             ;
-                        }
-                    });*/
-                    .setAdapter(adaptador, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int which) {
-
-                            System.out.println(which);
-                            // The 'which' argument contains the index position
-                            // of the selected item
                         }
                     });
                 return builder.create();
@@ -93,8 +82,8 @@ public class DialogFavoriteList extends DialogFragment {
 
         } catch (IOException e) {
             e.printStackTrace();
-        }
-        lista = new String[10];
+            }
+        lista = new String[0];
         return lista;
     }
 }
--- ucampas/app/src/main/java/zero/ucamaps/util/DialogFavoriteRoute.java
+++ ucampas/app/src/main/java/zero/ucamaps/util/DialogFavoriteRoute.java
@@ -3,6 +3,7 @@ package zero.ucamaps.util;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Environment;
@@ -12,7 +13,10 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
@@ -43,18 +47,55 @@ public class DialogFavoriteRoute extends DialogFragment {
 
                         final GlobalPoints globalVariable = (GlobalPoints) getActivity().getApplicationContext();
                         // Get name and email from global/application context
-                        final double latitude  = globalVariable.getLatitude();
+                        final double latitude = globalVariable.getLatitude();
                         final double longitude = globalVariable.getLongitude();
 
                         try {
-                            File tarjeta = Environment.getExternalStorageDirectory();
-                            File file = new File(tarjeta.getAbsolutePath(), "favorites_routes");
-                            OutputStreamWriter osw = new OutputStreamWriter(new FileOutputStream(file));
-                            osw.write(nombre_ruta + "_" + latitude + "_" + longitude);
-                            osw.flush();
-                            osw.close();
-                            //et1.setText("");
+                            //verificamos si llegamos a las 10 rutas limite
+                            int lineas = calcular_longitud();
+                            if (lineas >= 10) {
+
+                                new AlertDialog.Builder(getActivity())
+                                        .setTitle("Advertencia")
+                                        .setMessage("Ya tiene 10 rutas favoritas, para guardar una nueva, debe borrar una antigua, ¿desea continuar?")
+                                        .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                // continue with delete
+                                            }
+                                        })
+                                        .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                // do nothing
+                                            }
+                                        })
+                                        .setIcon(android.R.drawable.ic_dialog_alert)
+                                        .show();
+
+                            } else {
+                                File tarjeta = Environment.getExternalStorageDirectory();
+                                File file = new File(tarjeta.getAbsolutePath(), "favorites_routes");
+                                //verificamos si el archivo existe
+                                if (file.createNewFile()) {
+                                    Toast.makeText(getActivity(), "no habia archivo", Toast.LENGTH_SHORT).show();
+                                    //si la condicion da true, es por que el archivo no existia, y se creo, por ende, esta es la primera ruta creada
+                                    FileOutputStream fos = new FileOutputStream(file);
+                                    OutputStreamWriter osw = new OutputStreamWriter(fos);
+                                    osw.write(nombre_ruta + "_" + latitude + "_" + longitude + "\n");
+                                    osw.flush();
+                                    osw.close();
+                                    fos.close();
+                                    Toast.makeText(getActivity(), "Ruta Guardada Exitosamente", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(getActivity(), "si habia archivo", Toast.LENGTH_SHORT).show();
+                                    //si la consdicion da false, es por que el archivo ya existe, por ende se usa el filewrite
+                                    FileWriter fw = new FileWriter(file, true);
+                                    fw.write(nombre_ruta + "_" + latitude + "_" + longitude + "\n");
+                                    fw.close();
+                                    Toast.makeText(getActivity(), "Ruta Guardada Exitosamente", Toast.LENGTH_SHORT).show();
+                                }
+                            }
                         } catch (IOException ioe) {
+                            ioe.printStackTrace();
                         }
 
                     }
@@ -67,5 +108,29 @@ public class DialogFavoriteRoute extends DialogFragment {
                     }
                 });
         return builder.create();
+    }
+
+    private int calcular_longitud() throws IOException {
+        //cuenta las lineas del archivo para que no se pase de 10 rutas favoritas
+        File tarjeta = Environment.getExternalStorageDirectory();
+        File file = new File(tarjeta.getAbsolutePath(), "favorites_routes");
+        if(file.exists()){
+        FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader(fr);
+        String s;
+        int longitud = 0;
+        //leemos el archivo hasta el final, y aumentamos en 1 las lineas
+        while((s = br.readLine()) != null)
+            longitud++;
+
+        br.close();
+        br = null;      // Libera memoria
+        fr.close();
+        fr = null;      // Libera memoria
+        file = null; // Libera memoria
+         Toast.makeText(getActivity(), "ya conte las rutas", Toast.LENGTH_SHORT).show();
+        return longitud;
+        }else
+            return 0;
     }
 }
