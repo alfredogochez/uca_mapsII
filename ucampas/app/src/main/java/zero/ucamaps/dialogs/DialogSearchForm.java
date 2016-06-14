f05560ffdb1ebbdb9647404838b2809649828654
alfredogochez <00040211@uca.edu.sv>
2016-06-13 23:04:40 -0600

    agregado inicio de funcionalidad

--- ucampas/app/src/main/java/zero/ucamaps/dialogs/DialogSearchForm.java
+++ ucampas/app/src/main/java/zero/ucamaps/dialogs/DialogSearchForm.java
@@ -10,14 +10,16 @@ import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import zero.ucamaps.R;
 
 /**
  * Created by alf on 12/06/2016.
  */
-public class DialogSearchForm extends DialogFragment{
+public class DialogSearchForm extends DialogFragment {
 
 
 
@@ -29,29 +31,42 @@ public class DialogSearchForm extends DialogFragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
     Bundle savedInstanceState){
-        View vista = inflater.inflate(R.layout.search_form, container, false);
+        final View vista = inflater.inflate(R.layout.search_form, container, false);
         View titulo_busqueda = vista.findViewById(R.id.titulo_formulario);
         View pregunta = vista.findViewById(R.id.pregunta_formulario);
         View boton1 = vista.findViewById(R.id.rbSitio);
         View boton2 = vista.findViewById(R.id.rbPersonal);
         View boton3 = vista.findViewById(R.id.rbEdificio);
         View busqueda = vista.findViewById(R.id.txt_ingrese);
-        View txtbox = vista.findViewById(R.id.box_ingrese);
+        final View txtbox = vista.findViewById(R.id.box_ingrese);
         View boton = vista.findViewById(R.id.btn_buscar);
+        final RadioGroup grupo = (RadioGroup) vista.findViewById(R.id.rgBotones);
         //Seteando las cosas del formulario
         ((TextView) titulo_busqueda).setText("Formulario de Busqueda");
         ((TextView)pregunta).setText("¿Que desea buscar?");
         ((RadioButton)boton1).setText("Sitio");
         ((RadioButton)boton2).setText("Personal");
         ((RadioButton)boton3).setText("Edificio");
-        ((TextView)busqueda).setText("Ingrese su busqueda:");
+        ((TextView)busqueda).setText("Ingrese el nombre de lo que busca:");
         ((TextView)txtbox).setHint("...");
         ((Button)boton).setText("Buscar");
+        boton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(getActivity(),((TextView) txtbox).getText(), Toast.LENGTH_SHORT).show();
+                        int id = grupo.getCheckedRadioButtonId();
+                        RadioButton valBoton = (RadioButton) vista.findViewById(id);
+                        Toast.makeText(getActivity(),valBoton.getText(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
 
         getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
 
         return vista;
     }
+
 
 
 }
