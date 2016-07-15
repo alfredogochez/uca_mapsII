cf01c23af1156c8da427587fb290690da1837830
alfredogochez <00040211@uca.edu.sv>
2016-07-14 20:56:40 -0600

    modificada pantalla de about

--- ucampas/app/src/main/java/zero/ucamaps/dialogs/DialogInfoRoutes.java
+++ ucampas/app/src/main/java/zero/ucamaps/dialogs/DialogInfoRoutes.java
@@ -7,6 +7,7 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
