0d32db23d7dc2f4fbb20406b2a8cee120018e51a
uguizu <danii.uguizuz@gmail.com>
2016-09-22 20:39:37 -0600

    Se cambio string de My location por mi ubicacion

--- ucampas/app/src/main/java/zero/ucamaps/dialogs/NoteAdapter.java
+++ ucampas/app/src/main/java/zero/ucamaps/dialogs/NoteAdapter.java
@@ -114,7 +114,7 @@ public class NoteAdapter extends ArrayAdapter<Nota> {
                                 @Override
                                 public void onClick(DialogInterface dialog, int which) {
                                     //Sitio sitioEscogido = listaSitio.get(which);
-                                    mapFragment.onGetRouteMarked("My Location", nota.getEdificio());
+                                    mapFragment.onGetRouteMarked("Mi ubicacion", nota.getEdificio());
                                     Toast.makeText(getContext(), "Ruta trazada, ¡a por el!", Toast.LENGTH_SHORT).show();
                                     dialog.dismiss();
                                     dial.dismiss();
